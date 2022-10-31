f73904e127d30df026e3687ed1d0575afc815eef
Eric Scott <ericrscott@email.arizona.edu>
2022-10-31 19:15:18 +0000

    re-setup second run

--- MANDIFORE_runs/MANDIFORE-SEUS-3819/outdir/run/ENS-00001-1000002084/launcher.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-module load openmpi3; mpirun /groups/dlebauer/ed2_results/pecan/contrib/modellauncher/modellauncher /groups/dlebauer/ed2_results/pecan_remote/2022-10-27-21-41-32/run/ENS-00001-1000002084/joblist.txt
