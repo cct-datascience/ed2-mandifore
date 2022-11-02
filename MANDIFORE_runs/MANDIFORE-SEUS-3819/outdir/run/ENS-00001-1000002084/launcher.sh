90dd19e973ba7b6ce1344bab82971ae2c128a05b
Eric Scott <ericrscott@email.arizona.edu>
2022-11-02 14:09:49 +0000

    remove old runs

--- MANDIFORE_runs/MANDIFORE-SEUS-3819/outdir/run/ENS-00001-1000002084/launcher.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-module load openmpi3; mpirun /groups/dlebauer/ed2_results/pecan/contrib/modellauncher/modellauncher /groups/dlebauer/ed2_results/pecan_remote/2022-11-01-13-44-51/run/ENS-00001-1000002084/joblist.txt
