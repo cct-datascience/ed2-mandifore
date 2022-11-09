8f8b8177e39d8e16454c5e454247c22079f6322b
Eric Scott <ericrscott@email.arizona.edu>
2022-11-09 15:16:41 +0000

    run with simplified patch file

---
+++ MANDIFORE_runs/MANDIFORE-PNW-5909/outdir/run/ENS-00001-1000021575/launcher.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+module load openmpi3; mpirun /groups/dlebauer/ed2_results/pecan/contrib/modellauncher/modellauncher /groups/dlebauer/ed2_results/pecan_remote/2022-11-09-14-57-31/run/ENS-00001-1000021575/joblist.txt
