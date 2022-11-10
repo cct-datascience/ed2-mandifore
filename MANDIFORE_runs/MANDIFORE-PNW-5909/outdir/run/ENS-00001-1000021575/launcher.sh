a34e551d31f5735e009381e207d686431408ea5d
Eric Scott <scottericr@gmail.com>
2022-11-10 19:13:30 +0000

    gitignore outdir for easier review

--- MANDIFORE_runs/MANDIFORE-PNW-5909/outdir/run/ENS-00001-1000021575/launcher.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-module load openmpi3; mpirun /groups/dlebauer/ed2_results/pecan/contrib/modellauncher/modellauncher /groups/dlebauer/ed2_results/pecan_remote/2022-11-09-16-06-08/run/ENS-00001-1000021575/joblist.txt
