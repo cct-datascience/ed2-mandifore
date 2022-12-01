27ce37f9b8070802fffffddc64eebba3a9cea775
Eric Scott <scottericr@gmail.com>
2022-12-01 20:00:06 +0000

    whoops don't track completed runs

--- completed_runs/MANDIFORE-PNW-5909-complete/outdir/run/ENS-00001-1000021575/launcher.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-module load openmpi3; mpirun /groups/dlebauer/ed2_results/pecan/contrib/modellauncher/modellauncher /groups/dlebauer/ed2_results/pecan_remote/2022-11-16-15-31-09/run/ENS-00001-1000021575/joblist.txt
