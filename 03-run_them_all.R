211b77cc4e6970fbcc53dd8eca1b3d70fded0c75
Eric Scott <scottericr@gmail.com>
2022-12-02 14:02:43 +0000

    script to launch RStudio background jobs

---
+++ 03-run_them_all.R
@@ -0,0 +1,8 @@
+library(rstudioapi)
+library(purrr)
+library(here)
+
+workflows <- list.files("MANDIFORE_runs/", pattern = "workflow.R", recursive = TRUE, full.names = TRUE)
+
+#launch them all as RStudio jobs
+walk(workflows, ~jobRunScript(.x, workingDir = here()))
