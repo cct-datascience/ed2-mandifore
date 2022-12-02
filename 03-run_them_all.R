03dd2f40432926c1ce7a312f20411ebe66c88db0
Eric Scott <scottericr@gmail.com>
2022-12-02 16:01:04 +0000

    stagger job start by a few seconds

--- 03-run_them_all.R
+++ 03-run_them_all.R
@@ -5,4 +5,7 @@ library(here)
 workflows <- list.files("MANDIFORE_runs/", pattern = "workflow.R", recursive = TRUE, full.names = TRUE)
 
 #launch them all as RStudio jobs
-walk(workflows, ~jobRunScript(.x, workingDir = here()))
+walk(workflows, ~{
+  jobRunScript(.x, workingDir = here()) 
+  Sys.sleep(2) #stagger jobs
+})
