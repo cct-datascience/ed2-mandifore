9b584f42d343cd3de45a3abb8427a593cfe6587c
Eric Scott <scottericr@gmail.com>
2022-12-07 15:45:23 +0000

    run with callr

--- 03-run_them_all.R
+++ 03-run_them_all.R
@@ -1,21 +1,24 @@
-library(rstudioapi)
 library(purrr)
 library(here)
+library(furrr)
+future::plan(future.callr::callr, workers = 5)
 
+# get paths to all the workflow.R files
 workflows <- list.files("MANDIFORE_runs/", pattern = "workflow.R", recursive = TRUE, full.names = TRUE)
 
-# #launch them all as RStudio jobs
+# # launch them all as RStudio background jobs
+# library(rstudioapi)
 # walk(workflows, ~{
 #   jobRunScript(.x, workingDir = here()) 
 #   Sys.sleep(2) #stagger jobs
 # })
 
-# launch in separate R sessions with callr
-library(furrr)
-future::plan(future.callr::callr, workers = 5)
-future_walk(workflows, ~{
+# launch them all in separate R sessions and sink messages to a log
+future_walk(workflows, function(.x) {
+  #create a log file to capture logger messages
   logpath <- file.path(dirname(.x), "workflow_log.txt")
   sink(file(logpath, "wt"), type = "message")
   withr::defer(sink(file = NULL, type = "message"))
+  #source workflow.R
   source(.x)
 })
