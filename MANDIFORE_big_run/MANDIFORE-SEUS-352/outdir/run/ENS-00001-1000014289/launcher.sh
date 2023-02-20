fa8e4723c14d9a53d456f45a49654285a9028e93
Eric Scott <scottericr@gmail.com>
2023-02-20 20:18:14 +0000

    gitignore everything

--- MANDIFORE_big_run/MANDIFORE-SEUS-352/outdir/run/ENS-00001-1000014289/launcher.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-module load openmpi3; mpirun /groups/kristinariemer/ed2_results/pecan/contrib/modellauncher/modellauncher /groups/kristinariemer/ed2_results/pecan_remote/2023-02-15-19-11-58/run/ENS-00001-1000014289/joblist.txt
