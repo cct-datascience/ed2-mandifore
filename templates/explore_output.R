a7ce76932c0c7b58e152e79a89ef2cbc34fe259e
Eric Scott <scottericr@gmail.com>
2023-03-10 20:30:26 +0000

    plot ensemble runs

--- templates/explore_output.R
+++ templates/explore_output.R
@@ -2,31 +2,86 @@ library(tidync)
 library(fs)
 library(tidyverse)
 library(lubridate)
+library(ggforce)
+library(units)
+install_unit("plants", def = "unitless")
 
-source("R/extract_E_files.R")
+source("R/extract_nc_files.R")
 
-ensemble_data <- extract_E_files("MANDIFORE_big_run/MANDIFORE-SEUS-352/outdir/out/")
-write_csv(ensemble_data, "MANDIFORE_big_run/MANDIFORE-SEUS-352/ensemble_data.csv")
-ensemble_data <- read_csv("MANDIFORE_big_run/MANDIFORE-SEUS-352/ensemble_data.csv")
-ensemble_data <- 
-  ensemble_data |> 
+settings_path <- "/data/output/pecan_runs/MANDIFORE_big_run/MANDIFORE-SEUS-655/pine/settings_checked.xml"
+settings <- PEcAn.settings::read.settings(settings_path)
+
+# In case you want to re-run model2netcdf.ED2 locally for some reason
+# dir_ls(settings$modeloutdir, regexp = "ENS") |> 
+#   walk(~{
+#     model2netcdf.ED2(.x, settings = settings)
+#   })
+
+
+# Extract data from ensemble runs -----------------------------------------
+ens_data_raw <- 
+  dir_ls(settings$modeloutdir, regexp = "ENS") |> 
+  map(extract_run) |> 
+  bind_rows(.id = "filepath") |> 
+  mutate(run_id = path_file(filepath)) |> 
+  select(-filepath) |> 
+  select(run_id, date, everything()) 
+
+ens_data <- ens_data_raw |> 
   mutate(pft_name = case_when(
-  PFT==1  ~ "SetariaWT",
-  PFT==7  ~ "Southern_pine",
-  PFT==10 ~  "South_Mid_Hardwood",
-  PFT==8  ~ "Evergreen_Hardwood",
-  PFT==5  ~ "c3grass",
-  PFT==14 ~  "c4grass",
-  PFT==12 ~  "forb"
-))
+   pft == 1  ~ "SetariaWT",
+   pft == 7  ~ "Southern_pine",
+   pft == 10 ~  "South_Mid_Hardwood",
+   pft == 8  ~ "Evergreen_Hardwood",
+   pft == 5  ~ "c3grass",
+   pft == 14 ~  "c4grass",
+   pft == 12 ~  "forb"
+  )) |> 
+  # set units with {units}
+  mutate(
+    AGB_PFT = set_units(AGB_PFT, "kg/m^2"),
+    NPP_PFT = set_units(NPP_PFT, "kg/m^2/s"),
+    TRANSP_PFT = set_units(TRANSP_PFT, "kg/m^2/s"),
+    DENS = set_units(DENS, "plants/m^2"),
+    BSEEDS = set_units(BSEEDS, "kg/m^2"),
+    DBH = set_units(DBH, "cm")
+  )
+  
 
-agb_single_ens <- 
-  ensemble_data |> 
-  filter(ensemble == "ENS-00009-1000014289") |> 
-  group_by(patch, pft_name, date) |> 
-  summarize(AGB = sum(AGB_CO)) #add up cohorts within each patch/pft combo
+plot_df <- ens_data |>
+  group_by(date, pft_name) |> 
+  summarize(across(
+    c(AGB_PFT, NPP_PFT, TRANSP_PFT, DENS),
+    .fns = list(
+      mean = mean,
+      median = median,
+      lower = ~ quantile(., probs = 0.25),
+      upper = ~ quantile(., probs = 0.75)
+    )
+  ), .groups = "drop")
 
-agb_single_ens |> 
-  ggplot(aes(x = date, y = AGB)) +
-  geom_line(aes(color = pft_name)) +
-  facet_wrap(~patch, scales = "free_y", labeller = "label_both")
+ts_plot <- function(data, var, ylab) {
+  
+  data |> 
+    ggplot(aes(x = date, color = pft_name, fill = pft_name)) +
+    geom_line(aes_string(y = paste(var, "median", sep = "_"))) +
+    geom_ribbon(aes_string(ymin = paste(var, "lower", sep = "_"), ymax = paste(var, "upper", sep = "_")), alpha = 0.4, color = NA) +
+    labs(y = ylab, x = "Simulation Date", color = "PFT", fill = "PFT") +
+    theme_bw()
+}
+
+ts_plot(plot_df, c("Plant Density" = "DENS"))
+#take a named vector to plot them all
+x <- 
+  c(
+    "Plant density" = "DENS",
+    "Above-ground biomass" = "AGB_PFT",
+    "Leaf transpiration" = "TRANSP_PFT",
+    "Net primary productivity" = "NPP_PFT"
+  ) |> 
+  imap(~ts_plot(plot_df, .x, .y)) 
+
+library(patchwork)
+
+wrap_plots(x, ncol = 2) + plot_layout(guides = "collect")
+
