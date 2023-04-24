f37a8e6f845f87a5896129cd72eac176d5780fce
Eric Scott <scottericr@gmail.com>
2023-04-24 20:31:09 +0000

    new setup script for model training sites

---
+++ 02b-setup-seus-runs.R
@@ -0,0 +1,171 @@
+# load packages and functions -----------------------------------------------------------
+
+library(tidyverse)
+library(PEcAn.settings)
+library(fs)
+
+set.seed(4444)
+
+# load site info ----------------------------------------------------------
+# LHS sample of 20 sites
+sites <- read_csv("data/seus_sample20.csv")
+
+# 3 ecosystems corresponding to 3 .css files
+ecosystems <- c("pine", "mixed", "prairie")
+
+# create working directories ----------------------------------------------
+run_df <- 
+  expand_grid(sites, ecosystem = ecosystems) |> 
+  mutate(wd = path("seus_sample", sitename, ecosystem))
+
+#create all dirs
+dir_create(run_df$wd)
+
+#copy the same workflow template to all dirs
+walk(run_df$wd, \(x) {
+  file_copy(path("templates", "workflow_template_model_training.R"), path(x, "workflow.R"))
+})
+
+#copy job run script to all dirs
+walk(run_df$wd, \(x) {
+  file_copy(path("templates", "run.sh"), path(x, "run.sh"))
+})
+
+#copy pecan_xml per ecosystem
+run_df <- 
+  run_df |> 
+  mutate(template = path("templates", paste0("pecan_template_", ecosystem, ".xml"))) |> 
+  mutate(settings_path = file_copy(template, path(wd, "pecan.xml"))) |> 
+  #create paths in /data/ for outdir
+  mutate(outdir = path("/data/output/pecan_runs/seus_sample", sitename, ecosystem))
+
+# Filepaths ---------------------------------------------------------------
+# File paths for cohort and patch files.  The originals are in /data/input, and
+# they'll be moved to /data/sites/mandifore/<sitename>
+init <- 
+  run_df |> 
+  select(cohort_filename) |> 
+  filter(!duplicated(cohort_filename)) |> 
+  mutate(pss = dir_ls(path("/data/input", cohort_filename), glob = "*.pss"),
+         css = dir_ls(path("/data/input", cohort_filename), glob = "*.css"))
+
+# Now we need an altered filename for the files specifying the ecosystem
+# because each will be a different css file. Even though we are using the *same*
+# pss file for each ecosystem within a site, ED2 requries that the .pss and .css
+# filenames are the same, so we'll create unique paths for both and need to copy
+# over the .pss file three times under different names.
+run_df <- left_join(run_df, init, by = join_by(cohort_filename)) |> 
+  mutate(css_dest = path("/data/sites/mandifore/", sitename, paste(ecosystem, basename(css), sep = "_")),
+         pss_dest = path("/data/sites/mandifore/", sitename, paste(ecosystem, basename(pss), sep = "_")))
+
+# Customize pecan.xml -----------------------------------------------------
+
+settings_list <- map(run_df$settings_path, read.settings)
+
+# turn off ensemble analysis
+settings_list<- 
+  map(settings_list, \(x) {
+    x$ensemble <- NULL
+    compact(x) #removes NULLs
+  })
+
+# add data from sites tibble
+for (i in seq_len(nrow(run_df))) {
+  run <- run_df |> slice(i)
+  settings_list[[i]]$outdir <- run_df$outdir[i]
+  settings_list[[i]]$info$notes <- run_df$sitename[i]
+  settings_list[[i]]$run$site$id <- run_df$site_id[i]
+  settings_list[[i]]$run$site$met.start <- 
+    format(run_df$met_start_date[i], "%Y-%m-%d %H:%M:%S")
+  settings_list[[i]]$run$site$met.end <-
+    format(run_df$met_end_date[i], "%Y-%m-%d %H:%M:%S")
+  
+  settings_list[[i]]$run$inputs$met <-
+    fs::path("/data/sites/mandifore",
+             run_df$met_filename[i],
+             "ED_MET_DRIVER_HEADER")
+  # edit .css and .pss paths
+  settings_list[[i]]$run$inputs$pss <- run_df$pss_dest[i]
+  settings_list[[i]]$run$inputs$css <- run_df$css_dest[i]
+}
+
+
+# write settings out
+walk2(settings_list, run_df$wd, ~write.settings(.x, "pecan.xml", outputdir = .y))
+
+
+# Edit workflow.R ---------------------------------------------------------
+
+workflows <- map(run_df$wd, ~readLines(file.path(.x, "workflow.R")))
+settings_paths <- file.path(run_df$wd, "pecan.xml")
+
+map2(workflows, settings_paths, ~{
+  repl <- paste0('inputfile <- \"', .y, '\"')
+  str_replace(.x, 'inputfile <- .*', replacement = repl)
+}) |> 
+  walk2(run_df$wd, ~writeLines(.x, file.path(.y, "workflow.R")))
+
+
+# Modify ED_MET_DRIVER_HEADER -------------------------------------------------
+# Need to edit ED_MET_DRIVER_HEADER to point to correct path
+# 
+# E.g. change `/data/input/NARR_ED2_site_1-18168/` to `/data/sites/mandifore/NARR_ED2_site_1-18168/`
+# Skip files that are already done
+existing_met <- dir_ls("/data/sites/mandifore") |> basename()
+walk(sites$met_filename[!sites$met_filename %in% existing_met], ~{
+  file.copy(
+    from = file.path("/data/input", .x),
+    to = "/data/sites/mandifore",
+    recursive = TRUE,
+    copy.mode = FALSE
+  )
+  met_driver_path <-
+    file.path("/data/sites/mandifore",
+              .x,
+              "ED_MET_DRIVER_HEADER")
+  met_driver <- readLines(met_driver_path) #read in
+  met_driver <-
+    str_replace(met_driver, "/data/input/", "/data/sites/mandifore/") #fix path
+  write_lines(met_driver, met_driver_path)
+}, .progress = "Modify ED_MET_DRIVER_HEADER")
+
+
+# Copy files to HPC -------------------------------------------------------
+
+# MET files
+walk(sites$met_filename, #only need one met file per site, so can use the smaller df for this
+     \(met_filename) {
+       PEcAn.remote::remote.copy.to(
+         host = list(name = "puma"),
+         src = file.path("/data/sites/mandifore", met_filename),
+         dst = "/groups/kristinariemer/data/sites/mandifore"
+       )
+     }, .progress = "Copy MET files to HPC")
+
+# .css and .pss files
+
+#create directories
+walk(sites$sitename, \(x){
+  PEcAn.remote::remote.execute.cmd(
+    host = list(name = "puma"),
+    cmd = "mkdir",
+    args = path("/groups/kristinariemer/data/sites/mandifore/", x)
+  )
+})
+
+run_df |> 
+  rowwise() |> 
+  transmute(x = PEcAn.remote::remote.copy.to(
+    host = list(name = "puma"),
+    src = "/data/sites/generic_patches/generic.pss",
+    dst = path("/groups/kristinariemer", pss_dest)
+  ))
+
+run_df |> 
+  mutate(css = path("/data/sites/generic_patches", paste0(ecosystem, ".css"))) |> 
+  rowwise() |> 
+  transmute(x = PEcAn.remote::remote.copy.to(
+    host = list(name = "puma"),
+    src = css,
+    dst = path("/groups/kristinariemer", css_dest)
+  ))
---
+++ templates/workflow_template_model_training.R
@@ -0,0 +1,126 @@
+setwd(here::here()) #for run.sh to work. Shouldn't impact running interactively
+source(".Rprofile")
+# This may take a long time to run.  Run as a background job if you don't want
+# to tie up your R session.  In RStudio click the "Source" drop-down and choose
+# "Source as Local Job"
+
+# Load packages -----------------------------------------------------------
+# PEcAn.ED2 installed from PR https://github.com/PecanProject/pecan/pull/3125
+library(PEcAn.all) 
+library(purrr)
+library(stringr)
+library(fs)
+library(dplyr)
+library(readr)
+
+
+# Set logger level --------------------------------------------------------
+
+# Set to "INFO" so console isn't flooded with squeue messages
+olevel <- PEcAn.logger::logger.setLevel("INFO")
+
+# Read in settings --------------------------------------------------------
+
+#edit this path
+inputfile <- "ED2/MANDIFORE_runs/MANDIFORE-SEUS-xxxx/pecan.xml"
+settings <- PEcAn.settings::read.settings(inputfile)
+
+# Prepare settings --------------------------------------------------------
+settings <- prepare.settings(settings)
+settings <- do_conversions(settings)
+
+# Query trait database ----------------------------------------------------
+settings <- runModule.get.trait.data(settings)
+write.settings(settings, outputfile = "settings_checked.xml")
+
+# Meta analysis -----------------------------------------------------------
+#skip if this was already done
+exp_meta_files <- file.path(settings$pfts |> map_chr("outdir"), "trait.mcmc.Rdata")
+if(!all(file.exists(exp_meta_files))) {
+  runModule.run.meta.analysis(settings)
+}
+
+# Write model run configs -----------------------------------------------------
+
+## This will write config files locally.
+runModule.run.write.configs(settings)
+
+# Modify job.sh to run R inside singularity container.  
+# This is a workaround for https://github.com/PecanProject/pecan/issues/2540
+
+#this code also modifies the job.sh to run model2netcdf.ED2() with the
+#process_partial = TRUE option so that .nc files are created even for runs that
+#don't finish.
+
+job_scripts <-
+  list.files(settings$rundir,
+             "job.sh",
+             recursive = TRUE,
+             full.names = TRUE)
+#TODO: could get this from settings under the assumption that the .sh "ED
+#binary" has same naming convention as .sif file
+container_path <-
+  "/groups/kristinariemer/ed2_results/global_inputs/pecan-dev_ed2-dev.sif"
+
+purrr::walk(job_scripts, function(x) {
+  job_sh <- readLines(x)
+  cmd <-
+    paste0("singularity run ", container_path, " /usr/local/bin/Rscript")
+  job_sh_mod <- stringr::str_replace(job_sh, "Rscript", cmd)
+  # find which line has the model2netcdf.ED2() function
+  linenum <-
+    job_sh_mod |> str_detect("model2netcdf\\.ED2\\(") |> which()
+  # add process_partial = TRUE arg
+  job_sh_mod[linenum] <- job_sh_mod[linenum] |>
+    str_replace('\\)\\"$' , ', process_partial = TRUE\\)\\"')
+  writeLines(job_sh_mod, x)
+})
+
+## Remove all but focal runs
+# Only need to keep SA-median, -1SD for stomatal slope, -1SD for quantum efficiency, and +1SD for fineroot2leaf
+rundirs <- dir_ls(settings$rundir, type = "directory")
+modeloutdirs <- dir_ls(settings$modeloutdir, type = "directory")
+
+dir_ls(
+  settings$modeloutdir,
+  type = "directory", #only dirs
+  regexp = "SA-median$|stomatal_slope-0.159$|quantum_efficiency-0.159$|fineroot2leaf-0.841$",
+  invert = TRUE #all dirs that don't match regexp
+) |> file_delete()
+
+# Re-write runs.txt
+runs <- read_lines(path(settings$rundir, "runs.txt"))
+# This join is just to keep everything in the original order, in case that is
+# important.
+inner_join(
+  tibble(runs),
+  tibble(runs = dir_ls(settings$rundir, type = "directory") |> 
+           path_file())
+) |>
+  pull(runs) |>
+  write_lines(path(settings$rundir, "runs.txt"))
+
+
+# Start model runs --------------------------------------------------------
+
+## This copies config files to the HPC and starts the run
+runModule_start_model_runs(settings, stop.on.error = FALSE)
+
+# Cleanup -----------------------------------------------------------------
+# Remove .h5 files --------------------------------------------------------
+
+# To prevent Welsch from filling up, delete .h5 files if conversion to .nc was
+# successful.
+
+# runs that finished and had successful conversion of data
+end <- settings$run$end.date |> lubridate::year()
+done <- 
+  dir_ls(settings$modeloutdir, glob = paste0("*", end, ".nc"), recurse = TRUE) |>
+  path_dir()
+
+# and delete the .h5 files
+dir_ls(done, glob = "*.h5") |> file_delete()
+
+
+# Reset logger level to original value
+PEcAn.logger::logger.setLevel(olevel)
